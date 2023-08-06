2fb0597e17cd19ec55ad082676a00bb046ada5d8
Nyasha Chiwutsi <chiwutsi@gmail.com>
2023-08-07 01:48:16 +0200

    App init
    - Image upload + remove
    - drag images to canvas
    - move images around on canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Stage, Layer, Image } from "react-konva";
+import useImage from "use-image";
+import ImageUploading from "react-images-uploading";
 
-function App() {
+const URLImage = ({ image }) => {
+  const [img] = useImage(image.src);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Image
+      image={img}
+      x={image.x}
+      y={image.y}
+      offsetX={img ? img.width / 2 : 0}
+      offsetY={img ? img.height / 2 : 0}
+      draggable
+    />
+  );
+};
+
+const App = () => {
+  const dragUrl = React.useRef();
+  const stageRef = React.useRef();
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 10;
+  const onChange = (imageList) => {
+    setImages(imageList);
+  };
+
+  return (
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-3">
+        <h1 className="text-3xl font-bold ">Image Canvas</h1>
+        <ImageUploading
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+          acceptType={["png"]}
         >
-          Learn React
-        </a>
-      </header>
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            // write your building UI
+            <div className="upload__image-wrapper">
+              <button
+                style={isDragging ? { color: "red" } : null}
+                onClick={onImageUpload}
+                {...dragProps}
+                className="border border-black"
+              >
+                Click or Drop here
+              </button>
+              &nbsp;
+              <button
+                onClick={onImageRemoveAll}
+                className="border border-black"
+              >
+                Remove all images
+              </button>
+              {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                  <img
+                    src={image.data_url}
+                    alt=""
+                    width="200"
+                    draggable="true"
+                    onDragStart={(e) => {
+                      dragUrl.current = e.target.src;
+                    }}
+                  />
+                  <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageUpdate(index)}>Update</button>
+                    <button onClick={() => onImageRemove(index)}>Remove</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+        </ImageUploading>
+      </div>
+
+      <div
+        className="col-span-9"
+        onDrop={(e) => {
+          e.preventDefault();
+          stageRef.current.setPointersPositions(e);
+          setImages(
+            images.concat([
+              {
+                ...stageRef.current.getPointerPosition(),
+                src: dragUrl.current,
+              },
+            ])
+          );
+        }}
+        onDragOver={(e) => e.preventDefault()}
+      >
+        <Stage
+          width={window.innerWidth}
+          height={window.innerHeight}
+          style={{ border: "1px solid grey" }}
+          ref={stageRef}
+        >
+          <Layer>
+            {images.map((image, index) => {
+              return (
+                <URLImage
+                  image={image}
+                  key={index}
+                  style={{ width: "100px" }}
+                />
+              );
+            })}
+          </Layer>
+        </Stage>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
